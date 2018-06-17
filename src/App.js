4a2f260696f9daa4b11b0999357289a0b3f249cd
Chakradar Raju <chakradarraju@gmail.com>
2018-06-17 20:46:28 +0530

    Adding done state and filter based on that

--- src/App.js
+++ src/App.js
@@ -6,10 +6,26 @@ function DeleteButton(onClick) {
   return <input type='button' value='x' onClick={onClick} />
 }
 
-function Item(item, onClick) {
-  return (
-    <li key={item}>{DeleteButton(onClick)} {item}</li>
-  );
+class Item extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false,
+    };
+  }
+
+  toggleDone() {
+    this.setState({done: !this.state.done});
+  }
+
+  render() {
+    return (
+        ((this.state.done && this.props.showDone) || (!this.state.done && this.props.showPending)) && 
+        <li className={this.state.done ? 'done' : ''}
+            onClick={() => this.toggleDone()}>
+          {DeleteButton(this.props.onClick)} {this.props.item}
+        </li>);
+  }
 }
 
 class List extends Component {
@@ -25,14 +41,12 @@ class List extends Component {
     var newitem = this.state.newitem;
     if (!newitem || newitem.length == 0) return;
     this.state.items.push(newitem);
-    this.setState({
-      newitem: '',
-    });
+    this.setState({newitem: ''});
   }
 
   removeItem(i) {
     this.state.items.splice(i, 1);
-    this.forceUpdate();
+    this.setState({items: this.state.items});
   }
 
   onkeypress(e) {
@@ -51,7 +65,9 @@ class List extends Component {
         <input id='newitem' value={this.state.newitem} onChange={this.onchange} onKeyPress={e => this.onkeypress(e)} />
         <input type='button' value='Add' onClick={() => this.addItem()} />
         <ul id='list'>
-          {this.state.items.map(function(item, idx) { return Item(item, self.removeItem.bind(self, idx)); })}
+          {this.state.items.map(function(item, idx) {
+              return <Item showDone={self.props.showDone} showPending={self.props.showPending} key={item} item={item} onClick={self.removeItem.bind(self, idx)} />;
+          })}
         </ul>
       </div>
     );
@@ -59,11 +75,35 @@ class List extends Component {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDone: true,
+      showPending: true,
+    };
+  }
+
+  showAll() {
+    this.setState({showDone: true, showPending: true});
+  }
+
+  showDone() {
+    this.setState({showDone: true, showPending: false});
+  }
+
+  showPending() {
+    this.setState({showDone: false, showPending: true});
+  }
+
   render() {
     return (
-      <div>
-        <div>TODO</div>
-        <List></List>
+      <div id="app">
+        <div><h1>TODO</h1></div>
+        <input type="button" value="ALL" onClick={() => this.showAll()} />
+        <input type="button" value="DONE" onClick={() => this.showDone()} />
+        <input type="button" value="PENDING" onClick={() => this.showPending()} />
+        <br/>
+        <List showDone={this.state.showDone} showPending={this.state.showPending}></List>
       </div>
     );
   }
